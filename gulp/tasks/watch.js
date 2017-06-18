6529a6f0a154d23750a00f13f726c7ddb0761f40
Liv <livkao.lea@gmail.com>
2017-06-18 12:51:05 +0800

    Installed webpack and finished mobile menu

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,23 +3,32 @@ watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
-	notify: false,
 	browserSync.init({
+		notify: false,
 		server: {
 			baseDir: "app"
 		}
 	});
 
 	watch('./app/index.html', function() {
-		browserSync.reload()
-	})
+		browserSync.reload();
+	});
 
 	watch('./app/assets/styles/**/*.css', function() {
 		gulp.start('cssInject');
+	});
+
+	watch('./app/assets/scripts/**/*.js', function(){
+		gulp.start('scriptsRefresh');
 	})
 });
 
 gulp.task('cssInject', ['styles'],function(){
 	return gulp.src('./app/temp/styles/styles.css')
 		.pipe(browserSync.stream());
-})
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function(){
+	browserSync.reload();
+});
+
