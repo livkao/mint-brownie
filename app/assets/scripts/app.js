bd1f76bb21a0d223a743016fbf8868cc5bcf4396
Liv <livkao.lea@gmail.com>
2017-06-18 13:46:45 +0800

    Added reveal on scroll

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,4 +1,5 @@
 import MobileMenu from './modules/MobileMenu';
-
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
